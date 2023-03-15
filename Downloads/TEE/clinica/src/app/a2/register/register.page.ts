73ec60b0e44b6c3ceb9f42c791d92b37651cbbd1
“Gabriela” <Cordeiro.gabriela@outlook.com>
2023-03-14 21:13:35 -0300

    Aula 10/03

--- Downloads/TEE/clinica/src/app/a2/register/register.page.ts
+++ Downloads/TEE/clinica/src/app/a2/register/register.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +7,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.page.scss'],
 })
 export class RegisterPage implements OnInit {
+  public nome: string = '';
+  public sobrenome: string = '';
+  public nascimento: string = '';
+  public genero: string = '';
 
-  constructor() { }
+  constructor(
+    private nav: NavController
+  ) { }
 
   ngOnInit() {
+  }
+
+  back(){
+    this.nav.navigateBack('welcome');
+  }
+
+  send(){
+    console.log(this.nome);
+    console.log(this.sobrenome);
+    console.log(this.nascimento.toString().split('T')[0]);
+    console.log(this.genero);
+
+    const nome = this.nome;
+    const sobrenome = this.sobrenome;
+    const nascimento = this.nascimento.toString().split('T')[0];
+    const genero = this.genero;
+
+    const data = {nome, sobrenome, nascimento, genero};
+
+    console.log(data);
+
+    localStorage.setItem('pessoa', JSON.stringify(data));
+
+    
   }
 
 }
